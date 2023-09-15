2e14762765f2ebb464c189980090dfe52efcbbac
optimisme <optimisme@gmail.com>
2023-09-15 10:19:02 +0200

    First verstion

---
+++ Project/src/ObjCurs.java
@@ -0,0 +1,19 @@
+public class ObjCurs {
+
+    private int id;
+    private String nom;
+
+    public ObjCurs (int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    @Override
+    public String toString () {
+        return "Curs: " + this.id + " - " + this.nom;
+    }
+}
